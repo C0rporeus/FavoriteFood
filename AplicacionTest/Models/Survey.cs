4f209ca462c975dcfe4360e16125f3f4aa34db1b
Yonathan G <jhony2473@gmail.com>
2020-02-23 00:50:37 -0500

    adicion de proyectos

---
+++ AplicacionTest/Models/Survey.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AplicacionTest.Models
+{
+  public class Survey
+  {
+    public string Name{ get; set; }
+    public string FavoriteFoot { get; set; }
+  }
+}
---
+++ AplicacionTest/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AplicacionTest.ViewModels
+{
+  public class MainPageViewModel : NotificationObject
+  {
+
+  }
+}
